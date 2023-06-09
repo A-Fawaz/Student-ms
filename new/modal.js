2c1f3de9fdf7a03a123e58415052f07008ba90c6
Fatimatarhini <133159675+Fatimatarhini@users.noreply.github.com>
2023-06-09 13:20:49 +0300

    Revert "Revert "Revert "h"""
    This reverts commit 9a94cb686daca93a2f760f4f1f5a7c71e5471caf.

--- new/modal.js
+++ new/modal.js
@@ -1,10 +1,12 @@
-var modal = document.querySelector(".modal fade");
-   var btn = document.querySelector(".trigger-btn");
-  
+var modal = document.querySelector(".modal");
+   var btn = document.querySelector(".openModal");
+   var span = document.querySelector(".close");
    btn.addEventListener("click", () => {
       modal.style.display = "block";
    });
-   
+   span.addEventListener("click", () => {
+      hideModal();
+   });
    function hideModal() {
       modal.style.display = "none";
    }
@@ -19,7 +21,7 @@ var modal = document.querySelector(".modal fade");
 
     if (hidden) {
        element.removeAttribute("hidden");
-       
+       button.innerText = "Hide tr";
     } else {
        element.setAttribute("hidden", "hidden");
       
