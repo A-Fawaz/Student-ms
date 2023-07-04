1d97a77d82cdcbf5f092cca9b1971c36262e4ed2
Fatimatarhini <133159675+Fatimatarhini@users.noreply.github.com>
2023-07-04 23:54:50 +0300

    up

--- Teachers/class-management.php
+++ Teachers/class-management.php
@@ -1,49 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="class-management.css" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-      crossorigin="anonymous"
-    />
-    <scriptss
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-      crossorigin="anonymous"
-    ></script>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-      integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    <script>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="class-management.css" rel="stylesheet">
+  <title>Class Management</title>
+</head>
+<body>    <script>
       function openNav() {
         document.getElementById("mySidebar").style.width = "250px";
-        // document.getElementById("main").style.marginLeft = "250px";
-        // document.getElementById("container").style.marginLeft = "18vh";
-        document.getElementById("forcont").style.marginLeft = "50px";
+        // document.getElementById("forcont").style.marginLeft = "50px";
       }
 
       function closeNav() {
         document.getElementById("mySidebar").style.width = "0";
-        // document.getElementById("main").style.marginLeft = "0";
-        // document.getElementById("container").style.marginLeft = "0px";
-        document.getElementById("forcont").style.marginLeft = "0px";
+        // document.getElementById("forcont").style.marginLeft = "0px";
       }
     </script>
-
-    <title>Class Management</title>
-  </head>
-  <body>
-    <div class="containerF">
-    <aside>
+<aside>
         <div id="mySidebar" class="sidebar">
           <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"
             >&times;</a>
@@ -86,164 +60,47 @@
           <button class="openbtn" onclick="openNav()">&#9776;</button>
         </div>
       </aside>
-      <nav class="nav2">
         <h1 class="eschool1" id="eschool1">E-School</h1>
         
-    </nav>
     </div>
-    
-    <div class="for-cont" id="forcont">
-      <div class="row m-lg-5">
-        <div class="c Col-md-4 col-lg-4 col-xs-6 col-sm-6">
-          <div class="card">
+  <div id='full-cont'>
+</div>
+
+
+<script>
+  var ajax = new XMLHttpRequest();
+  var method = "GET";
+  var url = "classdata.php";
+  var asy = true;
+
+  ajax.open(method,url,asy);
+
+  ajax.send();
+
+  ajax.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      var data = JSON.parse(this.responseText);
+      console.log(data);
+
+      var html = "";
+      for(var a =0; a<data.length; a++){
+        var $classname = data[a].name;
+        var $classid = data[a].id;
+
+        html += ` <div class="card">
             <img src="../images/Teacher-student1.gif" class="card-img" />
             <div class="info-container">
-              <div class="grade-name">Grade-1</div>
-              <div class="student-number"><?php
-               $conn = mysqli_connect("localhost", "root", "", "school-management");
-
-              if (!$conn) {
-                  die('unable to connect to the database' . mysqli_error());
-              } else {
-                  $sql="SELECT count(*) as total FROM `student` WHERE classid='1'";
-                  $result = mysqli_query($conn, $sql);
-                  if(! $result) {
-                      echo "error";
-                  } else {
-                      while ($row = mysqli_fetch_assoc($result)) {
-                          echo $row['total'] .' '.'Students';
-                      }
-                  }
-
-              }
-
-              ?></div>
-             <a href="class-info.php?cid=1" ><button class="card-btn">View Class</button></a>
+              <div class="grade-name">`+$classname+`</div>
+              <div class="student-number"></div>
+              <a href="class-info.php?cid=`+$classid+`" ><button class="card-btn">View Class</button></a>
             </div>
-          </div>
-        </div>
+          </div>`;
+          
 
-        <div class="c Col-md-4 col-lg-4 col-xs-6 col-sm-6">
-          <div class="card">
-            <img src="../images/Teacher-student1.gif" class="card-img" />
-            <div class="info-container">
-              <div class="grade-name">Grade-2</div>
-              <div class="student-number"><?php $sql="SELECT count(*) as total FROM `student` WHERE classid='2'";
-              $result = mysqli_query($conn, $sql);
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo $row['total'] .' '.'Students';
-              }?></div>
-              <a href="class-info.php?cid=2" ><button class="card-btn">View Class</button></a>
-            </div>
-          </div>
-        </div>
-
-        <div class="c Col-md-4 col-lg-4 col-xs-6 col-sm-6">
-          <div class="card">
-            <img src="../images/Teacher-student1.gif" class="card-img" />
-            <div class="info-container">
-              <div class="grade-name">Grade-3</div>
-              <div class="student-number"><?php $sql="SELECT count(*) as total FROM `student` WHERE classid='3'";
-              $result = mysqli_query($conn, $sql);
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo $row['total'] .' '.'Students';
-              }?></div>
-              <a href="class-info.php?cid=3" ><button class="card-btn">View Class</button></a>
-            </div>
-          </div>
-        </div>
-
-        <div class="c Col-md-4 col-lg-4 col-xs-6 col-sm-6">
-          <div class="card">
-            <img src="../images/Teacher-student1.gif" class="card-img" />
-            <div class="info-container">
-              <div class="grade-name">Grade-4</div>
-              <div class="student-number"><?php $sql="SELECT count(*) as total FROM `student` WHERE classid='4'";
-              $result = mysqli_query($conn, $sql);
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo $row['total'] .' '.'Students';
-              }?></div>
-              <a href="class-info.php?cid=4" ><button class="card-btn">View Class</button></a>
-            </div>
-          </div>
-        </div>
-
-        <div class="c Col-md-4 col-lg-4 col-xs-6 col-sm-6">
-          <div class="card">
-            <img src="../images/Teacher-student1.gif" class="card-img" />
-            <div class="info-container">
-              <div class="grade-name">Grade-5</div>
-              <div class="student-number"><?php $sql="SELECT count(*) as total FROM `student` WHERE classid='5'";
-              $result = mysqli_query($conn, $sql);
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo $row['total'] .' '.'Students';
-              }?></div>
-              <a href="class-info.php?cid=5" ><button class="card-btn">View Class</button></a>
-            </div>
-          </div>
-        </div>
-
-        <div class="c Col-md-4 col-lg-4 col-xs-6 col-sm-6">
-          <div class="card">
-            <img src="../images/Teacher-student1.gif" class="card-img" />
-            <div class="info-container">
-              <div class="grade-name">Grade-6</div>
-              <div class="student-number"><?php $sql="SELECT count(*) as total FROM `student` WHERE classid='6'";
-              $result = mysqli_query($conn, $sql);
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo $row['total'] .' '.'Students';
-              }?></div>
-              <a href="class-info.php?cid=6" ><button class="card-btn">View Class</button></a>
-            </div>
-          </div>
-        </div>
-
-        <div class="c Col-md-4 col-lg-4 col-xs-6 col-sm-6">
-          <div class="card">
-            <img src="../images/Teacher-student1.gif" class="card-img" />
-            <div class="info-container">
-              <div class="grade-name">Grade-7</div>
-              <div class="student-number"><?php $sql="SELECT count(*) as total FROM `student` WHERE classid='7'";
-              $result = mysqli_query($conn, $sql);
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo $row['total'] .' '.'Students';
-              }?></div>
-              <a href="class-info.php?cid=7" ><button class="card-btn">View Class</button></a>
-            </div>
-          </div>
-        </div>
-
-        <div class="c Col-md-4 col-lg-4 col-xs-6 col-sm-6">
-          <div class="card">
-            <img src="../images/Teacher-student1.gif" class="card-img" />
-            <div class="info-container">
-              <div class="grade-name">Grade-8</div>
-              <div class="student-number"><?php $sql="SELECT count(*) as total FROM `student` WHERE classid='8'";
-              $result = mysqli_query($conn, $sql);
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo $row['total'] .' '.'Students';
-              }?></div>
-              <a href="class-info.php?cid=8" ><button class="card-btn">View Class</button></a>
-            </div>
-          </div>
-        </div>
-
-        <div class="c Col-md-4 col-lg-4 col-xs-6 col-sm-6">
-          <div class="card">
-            <img src="../images/Teacher-student1.gif" class="card-img" />
-            <div class="info-container">
-              <div class="grade-name">Grade-9</div>
-              <div class="student-number"><?php $sql="SELECT count(*) as total FROM `student` WHERE classid='9'";
-              $result = mysqli_query($conn, $sql);
-              while ($row = mysqli_fetch_assoc($result)) {
-                  echo $row['total'] .' '.'Students';
-              }?></div>
-              <a href="class-info.php?cid=9" ><button class="card-btn">View Class</button></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- </div> -->
-  </body>
+      }
+      document.getElementById('full-cont').innerHTML=html;
+    }
+  }
+</script>
+</body>
 </html>
